7cfa95d24a172fe5843e8aa11beb42b7959d9a68
João Vitor P.R. <jvprocha.05gmail.com>
2025-09-09 18:23:05 -0300

    Criando função Mostra_menu e suas opções

--- sprint.py
+++ sprint.py
@@ -1,1 +1,13 @@
-print("Passa bola")
+jogadoras = []
+times = []
+partidas = []
+
+def Mostra_menu():
+    print("Sistema de campeonatos\n"
+          "\n"
+          "1.Gerenciar Times\n"
+          "2.Gerenciar Jogadoras\n"
+          "3.Gerenciar Partidas\n"
+          "4.Relatórios\n"
+          "5.Sair\n")
+    return
